dd01b502f5553b3e791629f5bf85939c6e66a603
Jessica Foo <jessicafoo@dyn-118-139-53-142.its.monash.edu.au>
2017-03-28 16:23:13 +1100

    Revert "Revert "Merge branch 'coredata' into Save-Function""
    This reverts commit 4742bd6d5c050a96b9cc098f5f03bf3f2c553ff9.

---
+++ Darwins Monster Encyclopaedia/MonsterData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MonsterData+CoreDataClass.swift
+//  Darwins Monster Encyclopaedia
+//
+//  Created by Jessica Foo on 23/3/17.
+//  Copyright © 2017 Jessica Foo. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class MonsterData: NSManagedObject {
+
+}
